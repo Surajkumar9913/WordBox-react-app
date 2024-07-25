f81f412566e93c13a8b54bf4cf5018d4bdba9695
Surajkumar9913 <surajyuva200@gmail.com>
2024-07-25 22:35:34 +0530

    Some Changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -5,10 +5,12 @@ import Footer from './components/Footer'
 
 const Layout = () => {
   return (
-    <div>
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+    <div className='flex flex-col min-h-screen'>
+      <Navbar />
+      <main className='flex-grow'>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   )
 }
